134738ff84b0d4aae2cea88622615802fe784155
cvanbeck <charlievanbeck@gmail.com>
2022-02-23 23:41:24 +0000

    Working in console

--- javascript.js
+++ javascript.js
@@ -1,70 +1,67 @@
-
 function computerPlay() {
     roll = Math.random();
     if (roll < (1 / 3)) {
         hand = "rock";
     }
-    else if (roll >= (1/3) && roll < (2/3)) {
+    else if (roll >= (1 / 3) && roll < (2 / 3)) {
         hand = "paper";
     }
-    else if (roll >= (2/3)) {
+    else if (roll >= (2 / 3)) {
         hand = "scissors";
     }
     return hand
 }
 
-function playerChoice() {
-    choice = prompt("Rock paper or scissors").toLowerCase();
-
-    if (choice == "rock" || choice == "paper" || choice == "scissors") {
-        return choice;
-    }
-    else {
-        console.log("Please pick rock, paper, or scissors");
-        return 0
-    } 
-}
-
-function playRound(playerSelection, cpuSelection) {
-    if (playerSelection == cpuSelection) {
+function playRound(player) {
+    cpuSelection = computerPlay();
+    console.log(cpuSelection)
+    if (player == cpuSelection) {
+        console.log("draw");
         return "draw"
     }
-    else if ((playerSelection == "rock" && cpuSelection == "paper") || (playerSelection == "paper" && cpuSelection == "scissors") 
-                        || (playerSelection == "scissors"  && cpuSelection == "rock")) {
-             return "los"
-             }
-    else if ((playerSelection == "rock" && cpuSelection == "scissors" )|| (playerSelection == "paper" && cpuSelection == "rock") 
-                        || (playerSelection == "scissors"  && cpuSelection == "paper")) {
-             return "win"
-             }
-        
-        
+    else if ((player == "rock" && cpuSelection == "paper") || (player == "paper" && cpuSelection == "scissors")
+        || (player == "scissors" && cpuSelection == "rock")) {
+        console.log("lose");
+        ++gamesPlayed
+        return "lose"
+    }
+    else if ((player == "rock" && cpuSelection == "scissors") || (player == "paper" && cpuSelection == "rock")
+        || (player == "scissors" && cpuSelection == "paper")) {
+        console.log("win")
+        ++gamesPlayed
+        return "win"
+    }
 }
 
-function playFive() {
-    for (let i = 0; i < 5;) {
-        let player = playerChoice();
-        let computer = computerPlay();
-        console.log(`You chose ${player}!`);
-        console.log(`The computer chose ${computer}!`);
-        let outcome = playRound(player, computer);
-        
-        if (outcome == "win" ){
-            console.log(`You win, ${player} beats ${computer}.`);
-            ++i
-        }
-        else if (outcome == "lose") {
-            console.log(`You lose, ${computer} beats ${player}.`);
-            ++i
-        }
-        else {
-            console.log("It's a draw, play again.")
-        }
-    
-     }
-}
 
-const chooseHandPlease = document.querySelector(".choice");
-chooseHandPlease.addEventListener("click", () => {
-    alert("I should do the lesson proeprly :)")
+
+let gamesPlayed = 0;
+
+const chooseRock = document.querySelector("#rock");
+chooseRock.addEventListener("click", () => {
+    let playerChoice = "rock";
+    playRound(playerChoice)
+    console.log(gamesPlayed);
+    winChecker();
+
 })
+
+const choosePaper = document.querySelector("#paper");
+choosePaper.addEventListener("click", () => {
+    let playerChoice = "paper";
+    playRound(playerChoice)
+    ++gamesPlayed;
+})
+
+const chooseScissors = document.querySelector("#scissors");
+chooseScissors.addEventListener("click", () => {
+    let playerChoice = "scissors";
+    playRound(playerChoice);
+    ++gamesPlayed;
+})
+
+function winChecker() {
+    if (gamesPlayed % 5 == 0) {
+        console.log("You win!!!!!!")
+    }
+}
